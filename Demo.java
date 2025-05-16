835e0675c62ecda3d251903ba1b659f24489dc18
shashidhar001 <shashidharkr2001@gmail.com>
2025-05-16 12:10:19 +0530

    Updated code

--- Demo.java
+++ Demo.java
@@ -1,6 +1,15 @@
 public class Demo{
-	public static void main(String[] args){
-		String s1="Hello World";
-		System.out.println(" Get lost");
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the length of array:"+" ");
+		int n =sc.nextInt();
+		int  [] arr =new int [n];
+		System.out.println("enter the n values :"+" ");
+		for(int i=0;i< arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+
 	}
 }
